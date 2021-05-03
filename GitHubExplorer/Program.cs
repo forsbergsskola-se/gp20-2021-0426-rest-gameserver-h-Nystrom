43324ca88a45993d373add1993af3da8bfcb9f99
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-05-03 13:45:48 +0200

    Get html from a website

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.ComponentModel.Design;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace GitHubExplorer
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args){
+
+            var hello = GetHtmlFromWebsite("https://www.Github.com");
+            hello.Wait();
+            
+            
+            
+            Console.WriteLine("Press any key to quit!");
+            Console.ReadKey();
+        }
+
+        static async Task GetHtmlFromWebsite(string url){
+            var httpClient = new HttpClient();
+            try{
+                var response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e){
+                Console.WriteLine(e);
+            }
+            httpClient.Dispose();
         }
     }
 }
