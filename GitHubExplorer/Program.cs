d0afa9454e6d001b6858a576866f72702780947f
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-05-04 00:47:36 +0200

    Updates Login

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
 using GitHubExplorer.Data;
@@ -9,50 +8,103 @@ namespace GitHubExplorer
 {
     class Program{
 
-        const string GitHubUrl = "https://api.github.com/users/";
-        static HttpClient httpClient;
-        static string myUserName = "h-nystrom";
-        static string token;
-        // static User userProfile;
-
+        const string GitHubUrl = "https://api.github.com/";
+        static HttpClient _httpClient;
+        static string _uri;
+        static string _token;
+        static UserProfile _userProfile;
+        
         static void Main(string[] args){
+
+            if (!UserLogin()){
+                Console.Clear();
+                Console.WriteLine("Program is shutting down!");
+                return;
+            }
+            Console.WriteLine($"Welcome {_userProfile.login}!");
+            Console.WriteLine("Options:");
+            DisplayUserProfile();
+            UserProfileOptions();
             
-            // Console.Write("Enter your username: ");
-            // myUserName = Console.ReadLine();
-            
-            Console.Write("Enter gitHub api-key: ");
-            token = Console.ReadLine();
-            
-            // Console.Write("Search user: ");
-            // var userName = Console.ReadLine();
-            
-            var httpTask = GetHtmlFromWebsite("h-nystrom");
-            httpTask.Wait();
+            var requestTask = RequestUserProfile(_userProfile.repos_url);
+            requestTask.Wait();
+            Console.ReadKey();
 
             Console.WriteLine("Press any key to quit!");
             Console.ReadKey();
         }
 
-        static async Task GetHtmlFromWebsite(string userName){
-            httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.Add("Authorization", $"token {token}");
-            httpClient.BaseAddress = new Uri(GitHubUrl);
-            httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("GitHubExplorer1337", "1.0"));
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", token);
-            
+        static void DisplayUserProfile(){
+            Console.WriteLine(_userProfile.name);
+            Console.WriteLine(_userProfile.bio);
+            Console.WriteLine(_userProfile.location);
+            Console.WriteLine(_userProfile.twitter_username);
+            Console.WriteLine(_userProfile.email);
+            Console.WriteLine(_userProfile.blog);
+            Console.WriteLine(_userProfile.company);
+            Console.WriteLine(_userProfile.followers);
+            Console.WriteLine(_userProfile.following);
+            Console.WriteLine(_userProfile.public_repos);
+        }
+
+        static async Task RequestUserProfile(string uri){
             try{
-                var response = await httpClient.GetAsync($"{userName}");
+                _httpClient = new HttpClient().HeaderSetup(_token, GitHubUrl);
+                var response = await _httpClient.GetAsync($"{uri}");
                 response.EnsureSuccessStatusCode();
                 var responseBody = await response.Content.ReadAsStringAsync();
                 Console.WriteLine(responseBody);
-                var userInfo = JsonSerializer.Deserialize<UserData>(responseBody);
-                Console.WriteLine(userInfo?.bio);
+                _userProfile = JsonSerializer.Deserialize<UserProfile>(responseBody);
             }
             catch (HttpRequestException e){
-                Console.WriteLine(e);
+                Console.WriteLine($"Request failed: {e.StatusCode}");
+                throw;
             }
-            httpClient.Dispose();
+            _httpClient.Dispose();
+        }
+        static bool UserLogin(){
+            while (true){
+                    RequestUserInfo();
+                    try{
+                        var requestTask = RequestUserProfile($"users/{_uri}");
+                        requestTask.Wait();
+                        Console.Clear();
+                        return true;
+                    }
+                    catch (AggregateException e){
+                        Console.WriteLine("Press enter to try again!");
+                        if (GetConsoleKey() != ConsoleKey.Enter){
+                            return false;
+                        }
+                    }
+            }
+        }
+        static void RequestUserInfo(){
+            Console.Write("Enter your username: ");
+            _uri = Console.ReadLine();
+            Console.Write("Enter gitHub api-key: ");
+            _token = Console.ReadLine();
+        }
+
+        static void UserProfileOptions(){
+            switch (GetConsoleKey()){
+                case ConsoleKey.D0:
+                    Console.WriteLine("Option 0:");
+                    break;
+                case ConsoleKey.D1:
+                    Console.WriteLine("Option 1:");
+                    break;
+                case ConsoleKey.D2:
+                    Console.WriteLine("Option 2:");
+                    break;
+                default:
+                    Console.WriteLine("Unknown input");
+                    break;
+            }
+        }
+        static ConsoleKey GetConsoleKey(){
+            var keyInfo = Console.ReadKey();
+            return keyInfo.Key;
         }
     }
 }
