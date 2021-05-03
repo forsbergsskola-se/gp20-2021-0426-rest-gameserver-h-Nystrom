0be74ab951f2805b398b8a0219a7346fba10c3e1
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-05-03 17:09:05 +0200

    Updates program, now returns a json string from the http request.

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -1,27 +1,48 @@
 ﻿using System;
 using System.ComponentModel.Design;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace GitHubExplorer
 {
-    class Program
-    {
-        static void Main(string[] args){
+    class Program{
 
-            var hello = GetHtmlFromWebsite("https://www.Github.com");
-            hello.Wait();
+        const string GitHubUrl = "https://api.github.com/users/";
+        static HttpClient httpClient;
+        static string myUserName = "h-nystrom";
+        static string token;
+        // static User userProfile;
+
+        static void Main(string[] args){
             
+            // Console.Write("Enter your username: ");
+            // myUserName = Console.ReadLine();
             
+            Console.Write("Enter gitHub api-key: ");
+            token = Console.ReadLine();
             
+            // Console.Write("Search user: ");
+            // var userName = Console.ReadLine();
+            
+            var httpTask = GetHtmlFromWebsite("h-nystrom");
+            httpTask.Wait();
+
             Console.WriteLine("Press any key to quit!");
             Console.ReadKey();
         }
 
-        static async Task GetHtmlFromWebsite(string url){
-            var httpClient = new HttpClient();
+        static async Task GetHtmlFromWebsite(string userName){
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"token {token}");
+            httpClient.BaseAddress = new Uri(GitHubUrl);
+            httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("GitHubExplorer1337", "1.0"));
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", token);
+            
             try{
-                var response = await httpClient.GetAsync(url);
+                var response = await httpClient.GetAsync($"{userName}");
                 response.EnsureSuccessStatusCode();
                 var responseBody = await response.Content.ReadAsStringAsync();
                 Console.WriteLine(responseBody);
