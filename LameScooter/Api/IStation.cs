8ad4148c4a9e987ee9e7fda779647d2ed93f9170
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-05-06 12:43:52 +0200

    Adds Offline functionality

---
+++ LameScooter/Api/IStation.cs
@@ -0,0 +1,35 @@
+﻿namespace LameScooter.Api{
+    public interface IStation{
+        // int Id{ get; }
+        string Name{ get; }
+        // int X{ get; }
+        // int Y{ get; }
+        int BikesAvailable{ get; }
+        // int SpacesAvailable{ get; }
+        // int Capacity{ get; }
+        // bool AllowDropOff{ get; }
+        // bool AllowOverloading{ get; }
+        // bool IsFloatingBike{ get;}
+        // bool IsCarStation{ get; }
+        string State{ get; }
+        // string[] Networks{ get; }
+        // bool RealTimeData{ get; }
+    }
+
+    public class Station : IStation{
+        public int Id{ get; set; }
+        public string Name{ get; set;}
+        public int X{ get; set;}
+        public int Y{ get; set;}
+        public int BikesAvailable{ get; set;}
+        public int SpacesAvailable{ get; set;}
+        public int Capacity{ get; set;}
+        public bool AllowDropOff{ get; set;}
+        public bool AllowOverloading{ get; set;}
+        public bool IsFloatingBike{ get; set;}
+        public bool IsCarStation{ get; set;}
+        public string State{ get; set;}
+        public string[] Networks{ get; set;}
+        public bool RealTimeData{ get; set;}
+    }
+}
--- LameScooter/Api/OfflineLameScotterRental.cs
+++ LameScooter/Api/OfflineLameScotterRental.cs
@@ -1,9 +1,29 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace LameScooter.Api{
     public class OfflineLameScooterRental : ILameScooterRental{
+
+        readonly string databasePath = $"{Environment.CurrentDirectory}/DatabaseDummy/scooters.Json";
+        
         public Task<int> GetScooterCountInStation(string stationName){
-            throw new System.NotImplementedException();
+            try{
+                
+                var rawJson = File.ReadAllText(databasePath);
+                var stations = JsonConvert.DeserializeObject<Station[]>(rawJson);
+                foreach (var station in stations){
+                    if (station.Name == stationName){
+                        return Task.FromResult(station.BikesAvailable);
+                    }
+                }
+                throw new ArgumentException($"Exception: {stationName} doesn't exist!");
+            }
+            catch (Exception e){
+                Console.WriteLine(e);
+                throw new Exception(e.Message);
+            }
         }
     }
 }
