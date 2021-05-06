33d520cbf8ba294333ff5ccdb2d407021f03c799
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-05-06 13:05:02 +0200

    Update Program.cs

--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -6,8 +6,13 @@ namespace LameScooter{
     internal class Program{
         static async Task Main(string[] args){
             ILameScooterRental rental = new OfflineLameScooterRental();
-            var count = await rental.GetScooterCountInStation(args[0]);
-            Console.WriteLine($"Number of Scooters Available at this Station: {count}");
+            try{
+                var count = await rental.GetScooterCountInStation(args[0]);
+                Console.WriteLine($"Number of Scooters Available at this Station: {count}");
+            }
+            catch (Exception e){
+                Console.WriteLine(e.GetBaseException().Message);
+            }
         }
     }
 }
