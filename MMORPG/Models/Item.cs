0ecc7e35c139f203dde56f79a1fa6a918d2329a2
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-05-21 17:58:56 +0200

    Update Item.cs

--- MMORPG/Models/Item.cs
+++ MMORPG/Models/Item.cs
@@ -6,7 +6,7 @@ namespace MMORPG.Models{
     public class Item : IRequestObject{
         public Guid Id{ get; set; }
         public string Name{ get; set; }
-        public int Level{ get; set; }
+        public int ItemId{ get; set; }
         public string ItemType{ get; set; }//TODO: Fix to enum or something...
         public DateTime CreationDate{ get; set; }
         public int Gold{ get; set; }
